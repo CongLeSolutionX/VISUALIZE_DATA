05c036b4e7eb5cba98a7a196348893ea7a6fa70d
Cong Le <longchike@gmail.com>
2024-11-27 04:36:08 -0800

    add some more functions from the library

--- Rustworkx/Quickstart-demo.py
+++ Rustworkx/Quickstart-demo.py
@@ -11,6 +11,22 @@ c = graph.add_node("C")
 # and returns edge indices
 graph.add_edges_from([(a, b, 1.5), (a, c, 5.0), (b, c, 2.5)])
 
+
+# Examining elements of a graph
+node_indices = graph.node_indices()
+edge_indices = graph.edge_indices()
+print(node_indices)
+print(edge_indices)
+
+print("The first index data on the graph is:")
+first_index_data = graph[node_indices[0]]
+print(first_index_data)
+
+first_index_data = graph.get_edge_data_by_index(edge_indices[0])
+first_index_edgepoints = graph.get_edge_endpoints_by_index(edge_indices[0])
+print(first_index_edgepoints)
+print(first_index_data)
+
 # Returns the path A -> B -> C
 result = rx.dijkstra_shortest_paths(graph, a, c, weight_fn=float)
 print("The shortest path between A and C is:")
